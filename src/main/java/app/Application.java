67c18ce4c2970919edf9384097d957e4e42593e8
Kamil <kamil.arslanoffx@gmail.com>
2025-03-22 02:52:38 +0500

    коммит

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,19 +2,22 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Dog;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
+
+        applicationContext.close();
     }
 
 }
--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,21 @@
 package app.config;
 
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean
+    public Dog dog() {
+        return new Dog();
+    }
+    @Bean
+    public Timer timer() {
+        return new Timer();
+    }
 }
