fd1b893e27ca6bec3549d0336d723f79c3bef6a5
Kamil <kamil.arslanoffx@gmail.com>
2025-03-22 02:57:23 +0500

    коммит

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,12 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dog extends Animal {
-    private String name;
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return "Im a Dog";
     }
 }
